26fd7a8aa92b73c82dd91abd5eb33fb55f612df0
rdiachok <diachokroman@gmail.com>
2021-11-16 14:22:05 +0200

    initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/default/{name}", name="default")
+     */
+    public function index($name)
+    {
+        return $this->redirectToRoute('default2');
+    }
+
+   /**
+     * @Route("/default2/", name="default2")
+     */
+    public function index2()
+    {
+        return new Response('Hi!');
+    }
+}
